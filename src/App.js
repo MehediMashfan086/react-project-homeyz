2e6c5c975ccca5a81edefbb797decca294ef330a
Mehedi Hasan <89673282+MehediMashfan086@users.noreply.github.com>
2023-07-23 16:57:20 +0600

    feat(Companies): Companies Section Done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Companies from './components/Companies/Companies';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 
@@ -10,6 +11,7 @@ function App() {
         <Header />
         <Hero />
       </div>
+      <Companies />
     </div>
   );
 }
---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Companies.css"
+
+const Companies = () => {
+  return (
+    <section className="c-wrapper">
+        <div className="paddings flexCenter innerWidth c-container">
+            <img src="/images/prologis.png" alt="prologis-logo" />
+            <img src="/images/tower.png" alt="tower-logo" />
+            <img src="/images/equinix.png" alt="equinix-logo" />
+            <img src="/images/realty.png" alt="realty-logo" />
+        </div>
+    </section>
+  )
+}
+
+export default Companies
