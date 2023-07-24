f071287b5a8a2a55d5c44bae0ef7879ac80904a0
Mehedi Hasan <89673282+MehediMashfan086@users.noreply.github.com>
2023-07-24 15:18:01 +0600

    feat(responsive): Header Section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import "./Header.css"
+import OutsideClickHandler from 'react-outside-click-handler';
+import { BiMenuAltRight } from "react-icons/bi"
 
 const Header = () => {
   const [navbar, setNavbar] = useState(false)
+  const [menuOpened, setMenuOpened] = useState(false)
 
   const changeBackground = () => {
     if(window.scrollY >= 80) {
@@ -14,12 +17,23 @@ const Header = () => {
   }
 
   window.addEventListener("scroll", changeBackground)
+
+  const getMenuStyles = (menuOpened) => {
+    if(document.documentElement.clientWidth <= 800){
+      return {right: !menuOpened && "-100%"}
+    }
+  }
   return (
     <section className={navbar ? "navbar active" : "navbar"}>
         <div className="flexCenter paddings innerWidth h-container">
             <img src="/images/logo.png" alt="logo" width={100} />
+            <OutsideClickHandler 
+            onOutsideClick={()=> {
+              setMenuOpened(false)
+            }}
+            >
 
-            <div className="flexCenter h-menu">
+            <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
                 <a href="">Residencies</a>
                 <a href="">Our Value</a>
                 <a href="">Contact Us</a>
@@ -27,6 +41,10 @@ const Header = () => {
                 <button className="button">
                     <a href="">Contact</a>
                 </button>
+            </div>
+            </OutsideClickHandler>
+            <div className="menu-icon" onClick= {()=>setMenuOpened((prev)=>!prev)}>
+                <BiMenuAltRight size = {30} />
             </div>
         </div>
     </section>
