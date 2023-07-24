2ea450885adfe7a72c6bb72e4b1c6556989f1934
Mehedi Hasan <89673282+MehediMashfan086@users.noreply.github.com>
2023-07-24 15:09:30 +0600

    add scroll event feature on navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Header.css"
 
 const Header = () => {
+  const [navbar, setNavbar] = useState(false)
+
+  const changeBackground = () => {
+    if(window.scrollY >= 80) {
+      setNavbar(true)
+    }
+    else {
+      setNavbar(false)
+    }
+  }
+
+  window.addEventListener("scroll", changeBackground)
   return (
-    <section className="h-wrapper">
+    <section className={navbar ? "navbar active" : "navbar"}>
         <div className="flexCenter paddings innerWidth h-container">
             <img src="/images/logo.png" alt="logo" width={100} />
 
